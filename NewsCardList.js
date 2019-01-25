206187aa0b68d818f3af03a74798dc6c6b7a0a95
zubin | zeuscoder <zubin.pratap@gmail.com>
2019-01-25 23:09:03 +1100

    render dummy array as list using map function. WORKING

--- NewsCardList.js
+++ NewsCardList.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
-import NewsCardItem from './NewsCardItem'
+import React, { Component } from "react";
+import { Text, StyleSheet, View } from "react-native";
+import NewsCardItem from "./NewsCardItem";
 
 class NewsCardList extends Component {
-    
+  renderListOfNews = newsItems => {
+    return newsItems.map(item => {
+      return <NewsCardItem title={item.title} />;
+    });
+  };
+
   render() {
     return (
       <View>
-          <NewsCardItem title={this.props.newsItems[0].title}/>
+        {/* <NewsCardItem title={this.props.newsItems[0].title}/> */}
+        {this.renderListOfNews(this.props.newsItems)}
       </View>
-    )
+    );
   }
 }
 
 export default NewsCardList;
 
-const styles = StyleSheet.create({})
-
-
-  
+const styles = StyleSheet.create({});
